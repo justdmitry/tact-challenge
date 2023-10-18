54f08aa71c93051cda7771e6b3eb2e194154c1ff
Dmitry Popov <dmitry@parsec-club.ru>
2023-10-18 10:09:03 +0300

    Task 4: do not store seed

--- tests/Task4.spec.ts
+++ tests/Task4.spec.ts
@@ -1,14 +1,19 @@
 import { Blockchain, SandboxContract } from '@ton-community/sandbox';
+import '@ton-community/test-utils';
 import { toNano } from 'ton-core';
 import { Task4 } from '../wrappers/Task4';
+import { log } from 'console';
 
 describe('Task4', () => {
     let blockchain: Blockchain;
     let task4: SandboxContract<Task4>;
 
+    let seed1: bigint = 12345n;
+    let seed2: bigint = 1234567n;
+
     beforeEach(async () => {
         blockchain = await Blockchain.create();
-        task4 = blockchain.openContract(await Task4.fromInit());
+        task4 = blockchain.openContract(await Task4.fromInit(seed1));
         const deployer = await blockchain.treasury('deployer');
         const deployResult = await task4.send(
             deployer.getSender(),
@@ -28,7 +33,20 @@ describe('Task4', () => {
         });
     });
 
-    it('test', async () => {
+    it('have different addresses', async () => {
+        let task4v2: SandboxContract<Task4> = blockchain.openContract(await Task4.fromInit(seed1));
+        let task4v3: SandboxContract<Task4> = blockchain.openContract(await Task4.fromInit(seed2));
+        
+        let adr1: String = task4.address.toString();
+        let adr2: String = task4v2.address.toString();
+        let adr3: String = task4v3.address.toString();
+        
+        log("Task4 v1:", adr1);
+        log("Task4 v2:", adr2);
+        log("Task4 v3:", adr3);
+
+        expect(adr1).toEqual(adr2);
+        expect(adr1).not.toEqual(adr3);
     });
 });
 
