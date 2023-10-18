d1c588abc3d74555df3108ee9dbbdbafaedbd77c
Dmitry Popov <dmitry@parsec-club.ru>
2023-10-18 08:55:12 +0300

    Task 5: ignore deploy seed

--- tests/Task5.spec.ts
+++ tests/Task5.spec.ts
@@ -2,14 +2,19 @@ import { Blockchain, SandboxContract } from '@ton-community/sandbox';
 import '@ton-community/test-utils';
 import { toNano, Address } from 'ton-core';
 import { Task5 } from '../wrappers/Task5';
+import { log } from 'console';
 
 describe('Task5', () => {
     let blockchain: Blockchain;
     let task5: SandboxContract<Task5>;
+    
+    let seed1: bigint = 12345n;
+    let seed2: bigint = 1234567n;
+    let admin: Address = Address.parse("UQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAJKZ");
 
     beforeEach(async () => {
         blockchain = await Blockchain.create();
-        task5 = blockchain.openContract(await Task5.fromInit(123n, Address.parse("EQAEGkRSd_rLsrQZY07VuuHAIK5cQOpkwWwfCo0h9kLV7E6Q")));
+        task5 = blockchain.openContract(await Task5.fromInit(seed1, admin));
         const deployer = await blockchain.treasury('deployer');
         const deployResult = await task5.send(
             deployer.getSender(),
@@ -29,7 +34,20 @@ describe('Task5', () => {
         });
     });
 
-    it('test', async () => {
+    it('have different addresses', async () => {
+        let task5v2: SandboxContract<Task5> = blockchain.openContract(await Task5.fromInit(seed1, admin));
+        let task5v3: SandboxContract<Task5> = blockchain.openContract(await Task5.fromInit(seed2, admin));
+        
+        let adr1: String = task5.address.toString();
+        let adr2: String = task5v2.address.toString();
+        let adr3: String = task5v3.address.toString();
+        
+        log("Task5 v1:", adr1);
+        log("Task5 v2:", adr2);
+        log("Task5 v3:", adr3);
+
+        expect(adr1).toEqual(adr2);
+        expect(adr1).not.toEqual(adr3);
     });
 });
 
